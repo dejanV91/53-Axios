6976f587ef7eecb0e2d6d7cbe4dba334ddca418b
dejanV91 <dejanvukoicic91@gmail.com>
2022-12-05 18:06:12 +0100

    initial commit

--- src/examples/6-interceptors.js
+++ src/examples/6-interceptors.js
@@ -1,16 +1,24 @@
-import { useEffect } from 'react';
-
-const url = 'https://course-api.com/react-store-products';
+import { useEffect } from "react";
+import authFetch from "../axios/interceptors";
+const url = "https://course-api.com/react-store-products";
 
 const Interceptors = () => {
   const fetchData = async () => {
-    console.log('axios interceptors');
+    try {
+      const resp = await authFetch("/react-store-products");
+    } catch (error) {
+      console.log(error.response);
+      if (error.response.status === 404) {
+        console.log("NOT FOUND");
+      }
+      return Promise.reject(error);
+    }
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
-  return <h2 className='text-center'>interceptors</h2>;
+  return <h2 className="text-center">interceptors</h2>;
 };
 export default Interceptors;
